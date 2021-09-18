0397eeac6e1129ea47b834b8868948ab87b5a9a4
gregglegarda <gregglegarda@gwu.edu>
2021-09-18 13:00:03 -0400

    added posting capability

--- assets/js/marketpost.js
+++ assets/js/marketpost.js
@@ -1,1 +1,14 @@
 console.log("gregg");
+
+
+
+function submit_post() {
+    var n_jquery=$('#demo-message').val();
+    var n_js =document.getElementById('demo-message').value;
+    console.log("n_jquery",n_jquery);
+    console.log("n_js",n_js);
+}
+
+function clear_post() {
+    document.getElementById("demo-message").value = "";
+}
