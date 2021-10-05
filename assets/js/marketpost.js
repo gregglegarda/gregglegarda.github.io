856798a1f1d7fa210588685eb1a0691c87054e30
gregglegarda <gregglegarda@gwu.edu>
2021-10-04 20:41:26 -0400

    add aws capability, CallAPI

--- assets/js/marketpost.js
+++ assets/js/marketpost.js
@@ -2,34 +2,34 @@ console.log("marketpost.js");
 
 
 
+
+
+// define the callAPI function that takes a first name and last name as parameters
+var callAPI = (firstName,lastName)=>{
+     console.log("went here to callAPI");
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("https://osew5a4fd2.execute-api.us-east-1.amazonaws.com/dev6981", requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+}
+
+
+
 function submit_post(firstName,lastName) {
-
-    // define the callAPI function that takes a first name and last name as parameters
-    var callAPI = (firstName,lastName)=>{
-         console.log("went here to callAPI");
-        // instantiate a headers object
-        var myHeaders = new Headers();
-        // add content type header to object
-        myHeaders.append("Content-Type", "application/json");
-        // using built in JSON utility package turn object to string and store in a variable
-        var raw = JSON.stringify({"firstName":firstName,"lastName":lastName});
-        // create a JSON object with parameters for API call and store in a variable
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
-        // make API call with parameters and use promises to get response
-        fetch("https://osew5a4fd2.execute-api.us-east-1.amazonaws.com/dev6981", requestOptions)
-        .then(response => response.text())
-        .then(result => alert(JSON.parse(result).body))
-        .catch(error => console.log('error', error));
-    }
-
-
-
-
 
     var title = $('#demo-message-title').val();
     var description = $('#demo-message').val();
