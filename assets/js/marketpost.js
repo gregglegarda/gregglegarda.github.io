5952146d833f0e68df4621e4efafdb5f4698db68
gregglegarda <gregglegarda@gwu.edu>
2021-10-05 15:50:33 -0400

    add scan capability of database

--- assets/js/marketpost.js
+++ assets/js/marketpost.js
@@ -23,7 +23,7 @@ var callAPI = (firstName,lastName)=>{
     // make API call with parameters and use promises to get response
     fetch("https://osew5a4fd2.execute-api.us-east-1.amazonaws.com/dev6981", requestOptions)
     .then(response => response.text())
-    .then(result => console.log(JSON.parse(result).body))
+    .then(result => alert("JSON.parse(result).body"))
     .catch(error => console.log('error', error));
 
 
