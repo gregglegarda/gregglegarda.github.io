e0ff5b28c633c664517b4d8aefbe945f164a8662
gregglegarda <gregglegarda@gwu.edu>
2021-10-08 14:52:35 -0400

    add user name and alias prompt

--- assets/js/userstats.js
+++ assets/js/userstats.js
@@ -6,14 +6,16 @@ var current_guests;
 
 
 // define the callAPI function that takes a title and message as parameters
-var calluserstatsAPI = (fname, lname)=>{
+var calluserstatsAPI = (name)=>{
     console.log("calluserstatsAPI");
+    let new_date = new Date();
+    let date = new_date.toISOString();
     // instantiate a headers object
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
     // using built in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({"firstName":fname, "lastName":lname});
+    var raw = JSON.stringify({"name":name, "date":date});
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
         method: 'POST',
@@ -34,11 +36,9 @@ function get_name_alias(){
   let person = prompt("Please enter your name or alias:", "");
   if (person == null || person == "") {
     console.log("user cancelled");
-    alert("User cancelled the prompt.");
   } else {
     console.log("API called");
-    calluserstatsAPI(person, "legarday");
-    alert("Hello " + person + "! How are you today?");
+    calluserstatsAPI(person);
   }
 }
 
