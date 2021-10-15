8dedbb85c452d30dfd3ec05ce7d244d00df08c34
gregglegarda <gregglegarda@gwu.edu>
2021-10-14 21:19:10 -0400

    add imagemodal

--- assets/js/imagemodal.js
+++ assets/js/imagemodal.js
@@ -12,7 +12,7 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
