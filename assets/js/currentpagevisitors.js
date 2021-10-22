101b849c006946f8721736b1ec01046421ca962e
gregglegarda <gregglegarda@gwu.edu>
2021-10-21 21:50:36 -0400

    add current visitors

--- assets/js/currentpagevisitors.js
+++ assets/js/currentpagevisitors.js
@@ -1,6 +1,7 @@
+console.log("currentpagevisitors.js");
 var url = window.location.href;
 $.getJSON('https://api.gosquared.com/now/v3/pages?api_key=demo&site_token=GSN-106863-S&href=' + encodeURIComponent(url), function(data) {
-  print("visitors now: ", data.visitors);
+  console.log("visitors now: ", data.visitors);
   $('#online-now').addClass('visible').find('.visitors').text(data.visitors);
 
 });
